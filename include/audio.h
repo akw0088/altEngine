f23ac9050a115479f37322d10f6d517cdae6ded9
awright <akw0088@unt.edu>
2016-08-15 22:33:30 -0500

    add missing audio files

---
+++ include/audio.h
@@ -0,0 +1,35 @@
+#include "include.h"
+
+#ifndef AUDIO_H
+#define AUDIO_H
+
+class Audio
+{
+public:
+	void init();
+	void load(wave_t &wave);
+	void play(int hSource);
+	void stop(int hSource);
+	int create_source(bool loop, bool global);
+	void source_position(int hSource, float *position);
+	void source_velocity(int hSource, float *velocity);
+	void listener_position(float *position);
+	void listener_velocity(float *velocity);
+	void listener_orientation(float *orientation);
+	void delete_source(int hSource);
+	void select_buffer(int hSource, int hBuffer);
+	void delete_buffer(int hBuffer);
+	void destroy();
+	void effects(int source);
+private:
+	int checkFormat(char *data, char *format);
+	char *findChunk(char *chunk, char *id, int *size, char *end);
+	ALenum alFormat(wave_t *wave);
+
+	ALCdevice		*device;
+	ALCcontext		*context;
+	unsigned int	slot, effect, filter;
+
+};
+
+#endif
