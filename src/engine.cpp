8a597e3e1255018ed163a000a1070146a3b654e1
awright <akw0088@unt.edu>
2017-01-11 15:24:05 -0600

    prevent from picking up your teams flag

--- src/engine.cpp
+++ src/engine.cpp
@@ -1769,6 +1769,19 @@ void Engine::check_triggers(int self)
 		if (entity_list[i]->trigger->owner != -1 &&  entity_list[entity_list[i]->trigger->owner]->player->team == entity_list[self]->player->team && q3.gametype != GAMETYPE_DEATHMATCH)
 			continue;
 
+		if (strcmp(entity_list[i]->type, "blue_flag") == 0)
+		{
+			if (entity_list[self]->player->team == TEAM_BLUE)
+				continue;
+		}
+
+		if (strcmp(entity_list[i]->type, "red_flag") == 0)
+		{
+			if (entity_list[self]->player->team == TEAM_RED)
+				continue;
+		}
+
+
 		float distance = (entity_list[i]->position - entity_list[self]->position).magnitude();
 
 		if ( distance < entity_list[i]->trigger->radius)
