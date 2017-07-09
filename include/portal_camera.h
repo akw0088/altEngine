bcf0b22b9d82fa53eb696e605dd1152e3c441105
awright <akw0088@unt.edu>
2017-07-08 20:12:23 -0500

    Forgot to add portal camera files

---
+++ include/portal_camera.h
@@ -0,0 +1,19 @@
+#include "include.h"
+
+#ifndef PORTAL_CAMERA_H
+#define PORTAL_CAMERA_H
+
+class PortalCamera
+{
+public:
+	PortalCamera(Entity *entity, Graphics &gfx);
+	void destroy(Graphics &gfx);
+
+	Entity	*entity;
+	unsigned int fbo;
+	unsigned int quad_tex;
+	unsigned int depth_tex;
+	matrix4 portal_projection;
+};
+
+#endif
---
+++ src/portal_camera.cpp
@@ -0,0 +1,32 @@
+#include "portal_camera.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+PortalCamera::PortalCamera(Entity *entity, Graphics &gfx)
+{
+	PortalCamera::entity = entity;
+	quad_tex = -1;
+	depth_tex = -1;
+	fbo = -1;
+
+	portal_projection.perspective(90.0, 1.0, 1.0, 2001.0, false);
+#ifdef OPENGL
+	gfx.setupFramebuffer((int)(1024), (int)(1024), fbo, quad_tex, depth_tex, 0);
+#endif
+
+}
+
+void PortalCamera::destroy(Graphics &gfx)
+{
+	gfx.DeleteFrameBuffer(fbo);
+	gfx.DeleteTexture(quad_tex);
+	gfx.DeleteTexture(depth_tex);
+}
+
+
+
+
+
+
