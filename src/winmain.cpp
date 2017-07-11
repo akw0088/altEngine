3399b54fb9b6018c63301b8d9803c594d1ab318e
awright <akw0088@unt.edu>
2017-07-10 23:04:55 -0500

    Add GPU ram

--- src/winmain.cpp
+++ src/winmain.cpp
@@ -108,15 +108,34 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine,
 	printf("Vendor:\t\t%s\nStepping:\t%i\nModel:\t\t%i\nFamily:\t\t%i\nType:\t\t%i\nExtModel:\t%i\nExtFamily:\t%i\n", info.vendor, info.stepping,
 		info.model, info.family, info.type, info.extmodel,info.extfamily);
 
+	
+	ULONGLONG mem_kb = 0;
+	GetPhysicallyInstalledSystemMemory(&mem_kb);
+	printf("RAM:\t\t%.2f GB\n", (float)(mem_kb / (1024 * 1024)));
+
 #ifdef OPENGL
 	printf("OpenGL Version:\t%s\n", glGetString(GL_VERSION));
 	printf("GLSL Version:\t%s\n", glGetString(GL_SHADING_LANGUAGE_VERSION));
 	printf("Vendor:\t\t%s\n", glGetString(GL_VENDOR));
 	printf("GPU:\t\t%s\n", glGetString(GL_RENDERER));
+
+	if (strstr((char *)glGetString(GL_VENDOR), "nvidia") || strstr((char *)glGetString(GL_VENDOR), "NVIDIA"))
+	{
+#define GL_GPU_MEM_INFO_TOTAL_AVAILABLE_MEM_NVX 0x9048
+		int total_mem_kb = 0;
+		glGetIntegerv(GL_GPU_MEM_INFO_TOTAL_AVAILABLE_MEM_NVX, &total_mem_kb);
+		printf("GPU RAM:\t%.2f GB", (float)(total_mem_kb / (1024*1024)) );
+	}
+	else
+	{
+		UINT n = wglGetGPUIDsAMD(0, 0);
+		UINT *ids = new UINT[n];
+		size_t total_mem_mb = 0;
+		wglGetGPUIDsAMD(n, ids);
+		wglGetGPUInfoAMD(ids[0], WGL_GPU_RAM_AMD, GL_UNSIGNED_INT, sizeof(size_t), &total_mem_mb);
+		printf("GPU RAM:\t%.2f GB", (float)(total_mem_mb / 1024) );
+	}
 #endif
-	ULONGLONG mem_kb = 0;
-	GetPhysicallyInstalledSystemMemory(&mem_kb);
-	printf("RAM:\t\t%.2f GB\n", (float)(mem_kb / (1024 * 1024)));
 	
 
 
