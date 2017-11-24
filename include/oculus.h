4121ab38a17a049fc6f2ea438d5b1e60b47d53dd
awright <akw0088@unt.edu>
2017-11-23 18:42:06 -0600

    Add some Oculus virtual reality boilerplate

---
+++ include/oculus.h
@@ -0,0 +1,44 @@
+#include "include.h"
+#include "Extras/OVR_Math.h"
+#include "OVR_CAPI.h"
+#include "OVR_CAPI_GL.h"
+
+#ifndef OVR_H
+
+class Oculus
+{
+public:
+	int init(Graphics &gfx);
+	void get_pos(matrix3 &head, vec3 &position, matrix3 &lefthand, vec3 &left_pos, matrix3 &righthand, vec3 &right_pos);
+	void submit_frame();
+	int destroy();
+
+
+private:
+	int buffer(Graphics &gfx, int index);
+
+	ovrSession        session;
+	ovrHmdDesc        hmd_desc;
+	ovrEyeRenderDesc  render_desc[2];
+	ovrPosef          render_pose[2];
+	ovrVector3f       render_offset[2];
+
+	ovrMirrorTexture		mirror;
+	ovrMirrorTextureDesc	mirror_desc;
+	ovrTextureSwapChain		swap_chain;
+
+	ovrSizei   eye_size[2];
+	ovrPosef	eye_pose[2];
+	ovrTrackingState track;
+	double display_time;
+
+	unsigned int ovr_time;
+
+	// OpenGL specific
+	unsigned int ovr_fbo;
+	unsigned int ovr_depth;
+	unsigned int depth_tex;
+	unsigned int eye_tex[2];
+};
+
+#endif
---
+++ src/oculus.cpp
@@ -0,0 +1,141 @@
+#include "oculus.h"
+
+
+
+int Oculus::init(Graphics &gfx)
+{
+	ovrResult result = ovr_Initialize(NULL);
+	ovrGraphicsLuid luid;
+
+	if (result != ovrSuccess)
+	{
+		printf("Unable to initialize ovr\r\n");
+		return -1;
+	}
+
+	result = ovr_Create(&session, &luid);
+	if (result != ovrSuccess)
+	{
+		printf("Unable to create ovr\r\n");
+		return -1;
+	}
+
+	hmd_desc = ovr_GetHmdDesc(session);
+
+	buffer(gfx, 0);
+	buffer(gfx, 1);
+
+	return 0;
+}
+
+void Oculus::get_pos(matrix3 &head, vec3 &head_pos, matrix3 &lefthand, vec3 &left_pos, matrix3 &righthand, vec3 &right_pos)
+{
+	display_time = ovr_GetPredictedDisplayTime(session, 0);
+	track = ovr_GetTrackingState(session, ovr_time, ovrTrue);
+
+	head_pos.x = track.HeadPose.ThePose.Position.x;
+	head_pos.y = track.HeadPose.ThePose.Position.y;
+	head_pos.z = track.HeadPose.ThePose.Position.z;
+
+	left_pos.x = track.HandPoses[0].ThePose.Position.x;
+	left_pos.y = track.HandPoses[0].ThePose.Position.y;
+	left_pos.z = track.HandPoses[0].ThePose.Position.z;
+
+	right_pos.x = track.HandPoses[1].ThePose.Position.x;
+	right_pos.y = track.HandPoses[1].ThePose.Position.y;
+	right_pos.z = track.HandPoses[1].ThePose.Position.z;
+
+
+	//track.CalibratedOrigin.Orientation.x -- quaternion to matrix (matrix conversion on my other computer, tbd)
+
+	/*
+	track.CalibratedOrigin;
+	track.HandPoses;
+	track.HandStatusFlags;
+	track.HeadPose;
+	track.StatusFlags;
+	*/
+}
+
+
+
+int Oculus::buffer(Graphics &gfx, int index)
+{
+	ovrTextureSwapChainDesc desc;
+	int num_tex = 0;
+
+	eye_size[index] = ovr_GetFovTextureSize(session, (ovrEyeType)0, hmd_desc.DefaultEyeFov[index], 1.0f);
+
+	memset(&desc, 0, sizeof(desc));
+	desc.Type = ovrTexture_2D;
+	desc.ArraySize = 1;
+	desc.Width = eye_size[index].w;
+	desc.Height = eye_size[index].h;
+	desc.MipLevels = 1;
+	desc.Format = OVR_FORMAT_R8G8B8A8_UNORM_SRGB;
+	desc.SampleCount = 1;
+	desc.StaticImage = ovrFalse;
+
+	ovrResult result = ovr_CreateTextureSwapChainGL(session, &desc, &swap_chain);
+
+	ovr_GetTextureSwapChainLength(session, swap_chain, &num_tex);
+	for (int i = 0; i < num_tex; i++)
+	{
+		unsigned int chain_tex_id;
+		ovr_GetTextureSwapChainBufferGL(session, swap_chain, i, &chain_tex_id);
+		glBindTexture(GL_TEXTURE_2D, chain_tex_id);
+
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+	}
+
+	glGenFramebuffers(1, &ovr_fbo);
+	glGenTextures(1, &ovr_depth);
+	glBindTexture(GL_TEXTURE_2D, depth_tex);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT24, eye_size[index].w, eye_size[index].h, 0, GL_DEPTH_COMPONENT, GL_UNSIGNED_INT, NULL);
+
+	return 0;
+}
+
+void Oculus::submit_frame()
+{
+	ovrViewScaleDesc viewScaleDesc;
+	viewScaleDesc.HmdSpaceToWorldScaleInMeters = 1.0f;
+
+	ovrLayerEyeFov eyeLayer;
+	eyeLayer.Header.Type = ovrLayerType_EyeFov;
+	eyeLayer.Header.Flags = ovrLayerFlag_TextureOriginAtBottomLeft;
+
+	for (int i = 0; i < ovrEye_Count; i++)
+	{
+		eyeLayer.ColorTexture[i] = swap_chain;
+		eyeLayer.Viewport[i] = OVR::Recti(0, 0, eye_size[i].w, eye_size[i].h);
+		eyeLayer.Fov[i] = hmd_desc.DefaultEyeFov[i];
+		eyeLayer.RenderPose[i] = eye_pose[i];
+		eyeLayer.SensorSampleTime = ovr_time;
+	}
+
+	// append all the layers to global list
+	ovrLayerHeader* layerList = &eyeLayer.Header;
+
+	ovrResult result = ovr_SubmitFrame(session, ovr_time, NULL, &layerList, 1);
+}
+
+
+int Oculus::destroy()
+{
+	ovr_DestroyTextureSwapChain(session, swap_chain);
+	ovr_Destroy(session);
+	ovr_Shutdown();
+	return 0;
+}
+
+
