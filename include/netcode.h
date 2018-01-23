f2e97687bcd3ec843414ab3f93c625cc1a937266
awright <akw0088@unt.edu>
2018-01-22 21:02:54 -0600

    Add netcode class, eventually will move netcode into it

---
+++ include/netcode.h
@@ -0,0 +1,15 @@
+#include "include.h"
+
+#ifndef NETCODE_H
+#define NETCODE_H
+
+class Netcode
+{
+public:
+	Netcode();
+private:
+
+
+};
+
+#endif
---
+++ src/netcode.cpp
@@ -0,0 +1,9 @@
+#include "netcode.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+Netcode::Netcode()
+{
+}
