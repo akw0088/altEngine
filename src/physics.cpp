628401bbd1083025d46e546a612436868dfc221e
awright <akw0088@unt.edu>
2018-01-26 23:50:33 -0600

    Add capsule intersection tests (line segments)

--- src/physics.cpp
+++ src/physics.cpp
@@ -4373,3 +4373,117 @@ void PhysicsSystem::Update(float deltaTime)
 		}
 	}
 }
+
+// get the minimum distance between 2 line segments
+float distance_segment_segment(line3_t &s1, line3_t &s2)
+{
+	vec3	u = s1.b - s1.a;
+	vec3	v = s2.b - s2.a;
+	vec3	w = s1.a - s2.a;
+	float	a = u * u;
+	float	b = u * v;
+	float	c = v * v;
+	float	d = u * w;
+	float	e = v * w;
+	float	D = a *  c - b*  b;
+	float	sc, sN;
+	float	sD = D;		// sc = sN / sD, default sD = D >= 0
+	float	tc, tN;
+	float	tD = D;		// tc = tN / tD, default tD = D >= 0
+
+	// compute the line parameters of the two closest points
+	if (D < 0.001f)
+	{
+		// the lines are almost parallel
+		sN = 0.0;         // force using point P0 on segment S1
+		sD = 1.0;         // to prevent possible division by 0.0 later
+		tN = e;
+		tD = c;
+	}
+	else
+	{
+		// get the closest points on the infinite lines
+		sN = b * e - c * d;
+		tN = a * e - b * d;
+		if (sN < 0.0)
+		{
+			// sc < 0 => the s=0 edge is visible
+			sN = 0.0;
+			tN = e;
+			tD = c;
+		}
+		else if (sN > sD)
+		{
+			// sc > 1  => the s=1 edge is visible
+			sN = sD;
+			tN = e + b;
+			tD = c;
+		}
+	}
+
+	if (tN < 0.0)
+	{            // tc < 0 => the t=0 edge is visible
+		tN = 0.0;
+		// recompute sc for this edge
+		if (-d < 0.0)
+		{
+			sN = 0.0;
+		}
+		else if (-d > a)
+		{
+			sN = sD;
+		}
+		else
+		{
+			sN = -d;
+			sD = a;
+		}
+	}
+	else if (tN > tD)
+	{
+		// tc > 1  => the t=1 edge is visible
+		tN = tD;
+		// recompute sc for this edge
+		if ((-d + b) < 0.0)
+		{
+			sN = 0;
+		}
+		else if ((-d + b) > a)
+		{
+			sN = sD;
+		}
+		else
+		{
+			sN = (-d + b);
+			sD = a;
+		}
+	}
+	// finally do the division to get sc and tc
+	sc = (abs32(sN) < 0.001f ? 0.0 : sN / sD);
+	tc = (abs32(tN) < 0.001f ? 0.0 : tN / tD);
+
+	// get the difference of the two closest points
+	vec3 dP = w + (u * sc) - (v * tc);  // =  S1(sc) - S2(tc)
+
+	return dP.magnitude();   // return the closest distance
+}
+
+bool IntersectCapsule(vec3 &eye, vec3 &dir, vec3 &capsule_a, vec3 &capsule_b, float radius)
+{
+	vec3 end = eye + (dir * 8192);
+	line3_t segment1;
+	line3_t segment2;
+
+	segment1.a = eye;
+	segment1.b = end;
+	segment2.a = capsule_a;
+	segment2.b = capsule_b;
+	float dist = distance_segment_segment(segment1, segment2);
+
+	if (dist < radius)
+	{
+		return true;
+	}
+
+	return false;
+}
