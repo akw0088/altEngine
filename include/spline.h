f9e4f2da8a79da115c8ed1dd52e3bfc8ebf3b27c
awright <akw0088@unt.edu>
2018-06-16 23:54:30 -0500

    Forgot files

--- include/spline.h
+++ include/spline.h
@@ -10,9 +10,16 @@ public:
 	void load(char *file);
 	void destroy();
 	void step(Frame &camera_frame, float t);
+
 	~Spline();
 
 private:
+	float HermiteInterpolate(float y0, float y1, float y2, float y3,
+		float mu, float tension, float bias);
+	vec3 HermiteInterp(vec3 &a, vec3 &b, vec3 &c, vec3 &d, float t);
+	vec3 para_spline(vec3 *control, int num_control, float t);
+
+
 	vec3 control[32];
 	quaternion controlq[32];
 	int num_control;
--- src/spline.cpp
+++ src/spline.cpp
@@ -64,6 +64,60 @@ void Spline::step(Frame &camera_frame, float t)
 	camera_frame.pos = pos;
 }
 
+float Spline::HermiteInterpolate(float y0, float y1, float y2, float y3,
+	float mu, float tension, float bias)
+{
+	float m0, m1, mu2, mu3;
+	float a0, a1, a2, a3;
+
+	mu2 = mu * mu;
+	mu3 = mu2 * mu;
+	m0 = (y1 - y0)*(1 + bias)*(1 - tension) / 2;
+	m0 += (y2 - y1)*(1 - bias)*(1 - tension) / 2;
+	m1 = (y2 - y1)*(1 + bias)*(1 - tension) / 2;
+	m1 += (y3 - y2)*(1 - bias)*(1 - tension) / 2;
+	a0 = 2 * mu3 - 3 * mu2 + 1;
+	a1 = mu3 - 2 * mu2 + mu;
+	a2 = mu3 - mu2;
+	a3 = -2 * mu3 + 3 * mu2;
+
+	return(a0*y1 + a1*m0 + a2*m1 + a3*y2);
+}
+
+
+vec3 Spline::HermiteInterp(vec3 &a, vec3 &b, vec3 &c, vec3 &d, float t)
+{
+	vec3 result;
+
+	result.x = HermiteInterpolate(a.x, b.x, c.x, d.x, t, 0.0f, 0.0f);
+	result.y = HermiteInterpolate(a.y, b.y, c.y, d.y, t, 0.0f, 0.0f);
+	result.z = HermiteInterpolate(a.z, b.z, c.z, d.z, t, 0.0f, 0.0f);
+	return result;
+}
+
+
+vec3 Spline::para_spline(vec3 *control, int num_control, float t)
+{
+	int seg = t * (num_control - 1);
+
+	t = clamp(t, 0.0f, 1.0f);
+	float nt = (t * (num_control - 1)) - ((int)(t * (num_control - 1)) % (num_control - 1));
+
+
+	if (seg == 0)
+	{
+		return HermiteInterp(control[0], control[0], control[1], control[2], nt);
+	}
+	else if (seg < num_control - 2)
+	{
+		return HermiteInterp(control[seg - 1], control[seg], control[seg + 1], control[seg + 2], nt);
+	}
+	else
+	{
+		return HermiteInterp(control[num_control - 3], control[num_control - 2], control[num_control - 1], control[num_control - 1], nt);
+	}
+}
+
 void Spline::destroy()
 {
 	num_control = 0;
