0b4718429a3f463ebd7225f082d4f9d89515b4df
awright <akw0088@unt.edu>
2018-08-31 16:43:55 -0500

    add more vm code

--- vm/g_main.c
+++ vm/g_main.c
@@ -1,31 +1,61 @@
 #include "bg_lib.h"
 
-void printf(const char* fmt, ...);
+#define GAMEVERSION "1.0.0"
+
+void G_Printf(const char* fmt, ...);
+
+void G_InitGame();
+void G_StepGame();
+
+
+enum {
+	G_INIT,
+	G_STEP,
+	G_DESTROY
+};
+
 
 int vmMain(int command, int arg0, int arg1, int arg2, int arg3, int arg4,
            int arg5, int arg6, int arg7, int arg8, int arg9, int arg10,
            int arg11)
 {
-    if (command == 0)
-    {
-        printf("Game init!\n");
-    }
-    else
-    {
-        printf("Unknown command.\n");
-    }
+	switch (command)
+	{
+	case G_STEP:
+		G_StepGame();
+		break;
+	case G_INIT:
+		G_InitGame();
+		break;
+	case G_DESTROY:
+		break;
+	default:
+		G_Printf("Unknown command %s\n");
+	}
+
 
     return 0;
 }
 
-void printf(const char* fmt, ...)
+void G_Printf(const char* fmt, ...)
 {
-    va_list argptr;
-    char    text[1024];
+	va_list argptr;
+	char    text[1024];
 
-    va_start(argptr, fmt);
-    vsprintf(text, fmt, argptr);
-    va_end(argptr);
+	va_start(argptr, fmt);
+	vsprintf(text, fmt, argptr);
+	va_end(argptr);
 
-    trap_Printf(text);
+	trap_Printf(text);
+}
+
+void G_InitGame()
+{
+	G_Printf("------- Game Initialization -------\n");
+	G_Printf("gamename: %s\n", GAMEVERSION);
+	G_Printf("gamedate: %s\n", __DATE__);
+}
+
+void G_StepGame()
+{
 }
