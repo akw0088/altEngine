9c3b42708946a22529f6cdd35b1e4db617e0a59c
awright <akw0088@unt.edu>
2020-12-02 20:30:35 -0600

    Add function headers to entity.cpp

--- src/entity.cpp
+++ src/entity.cpp
@@ -12,12 +12,56 @@
 // DEALINGS IN THE SOFTWARE.
 //=============================================================================
 
+///============================================================================
+/// File: entity.cpp
+///============================================================================
+/// Class for entity definition
+///
+/// So the entities are stored as a fixed size array
+/// first 0-16 are reserved for players
+/// next section is map entities
+/// last section is reserved for dynamic entities (bullet casing, rockets,
+/// grenades etc)
+///
+/// Instead of using inheritance (which becomes messy) this base class
+/// simply stores pointers to every subclass possible
+/// This also allows for a single entity to have multiple properties
+/// Eg: a sound source that emits light
+/// a Model that emits sound, light, and triggers an event when near etc
+///
+/// The entity itself is just composed of a position in space
+/// (although now it looks like some basic flags have crept in)
+/// But the idea is to store most everything in the sub classes
+///
+/// The sub classes access this data through an Entity pointer passed to them
+/// at construction
+///
+/// The entity list is a fixed size, but the pointers are currently dynamically
+/// allocated, we could use fixed size arrays for each type, but memory leaking
+/// isnt too big of a concern as we have pretty awesome debug tools nowadays
+/// between visual studio and clang sanitizers. Hitching during allocation
+/// doesn't seem to be an issue in my experience
+///============================================================================
+
 #include "entity.h"
 
 #ifdef _DEBUG
 #define new DEBUG_NEW
 #endif
 
+
+///=============================================================================
+/// Function: Entity
+///=============================================================================
+/// Description: Constructor for Entity class
+///
+///
+/// Parameters:
+///		None
+///
+/// Returns:
+///		None
+///=============================================================================
 Entity::Entity()
 {
 	position = vec3(0.0f, 0.0f, 0.0f);
